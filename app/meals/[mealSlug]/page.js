cbf856e1ff9c6dc140a5470c87e5c4c9899bd4a2
Christophar Antony <christopharantony@gmail.com>
2024-10-20 12:29:09 +0530

    Meal details page Integration with not found handling

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,26 +1,35 @@
-import Link from "next/link";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-export default function MealDetails({params}) {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
+export default function MealDetails({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) notFound();
+
+  const instructions = meal.instructions.replaceAll("\n", "<br />");
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Meal {params.id} Details
-      </h1>
-      <div style={{display: 'flex', justifyContent: 'center', margin: '2rem', marginTop: '8rem'}}>
-        <Link href="/meals">Meals</Link>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center', margin: '2rem', }}>
-        <Link href="/meals/share">Share Meals</Link>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center', margin: '2rem', }}>
-        <Link href="/community">Community</Link>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center', margin: '2rem', }}>
-        <Link href="/meals/1">Meal 1</Link>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center', margin: '2rem', }}>
-        <Link href="/meals/fav">Favorite Meals</Link>
-      </div>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
