e79345971e6fc0d30512624fae4c73c58d0655f0
Christophar Antony <christopharantony@gmail.com>
2024-10-24 18:24:01 +0530

    Add server side input validation for meal sharing and error component for failed submissions

---
+++ app/meals/share/error.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An error occured.</h1>
+      <p>Failed to share your recipe. Please try again later</p>
+    </main>
+  );
+};
+
+export default Error;
